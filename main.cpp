bc5e8017dacdbd8303845b1fa8de25e0d86cb857
cauashmith <contatocaua.shmith@gmail.com>
2024-11-24 23:43:28 -0300

    funcao randomica para dado

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <random>
+using namespace std;
+
+int randomValue();
 
 int main() {
-    std::cout << "Hello World!";
+    int randomV = randomValue();
+    cout << "Numero que caiu no dado: " << randomV << endl;
     return 0;
+}
+
+int randomValue() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 10);
+
+    int random_number = dis(gen);
+
+    return random_number;
 }
 
