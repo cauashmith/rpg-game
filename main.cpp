8393b56305b03b2baa3d5e6305d76139d3e30852
fbnGametor <trindadefabio2016@gmail.com>
2024-11-25 22:28:11 -0300

    Criando struct Personagem e Monstro

--- main.cpp
+++ main.cpp
@@ -4,6 +4,34 @@ using namespace std;
 
 int randomValue();
 
+struct Personagem {
+    string nome;
+    string classe;
+    int vida;
+    int defesa;
+    int level;
+    int founded;
+    unordered_map<string, int> ataques;
+
+    void mostrarAtaques() const {
+        cout << "" << endl;
+        cout << "Ataques disponiveis para " << nome << ":" << endl;
+        int count = 1;
+        for (const auto& ataque : ataques) {
+            cout << count << ". - " << ataque.first << ": Dano " << ataque.second << endl;
+            count++;
+        }
+        cout << "" << endl;
+    }
+};
+
+struct Monstro {
+    string nome;
+    int vida;
+    int danoMin;
+    int danoMax;
+};
+
 int main() {
     int randomV = randomValue();
     cout << "Numero que caiu no dado: " << randomV << endl;
@@ -19,5 +47,3 @@ int randomValue() {
 
     return random_number;
 }
-
-// igor
